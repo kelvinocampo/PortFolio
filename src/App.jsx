63b3d754c13ad9132b2e56a6c582b12faf209dbb
kelvinocampo <kevinocampooso@gmail.com>
2025-12-09 09:30:44 -0500

    feat: implement routing for language selection and add English data support

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import { Experience } from "@/components/Experience"
 import { Skills } from "@/components/Skills"
 import { Projects } from "@/components/Projects"
 import { Contact } from "@/components/Contact"
+import { Navigate, Route, Routes } from "react-router"
 
 export const App = () => {
   return (
-    <>
-      <Header />
-      <Experience />
-      <Skills />
-      <Projects />
-      <Contact />
-    </>
+    <Routes>
+      <Route path="/" element={<Navigate to="/es" replace />} />
+      <Route path="/:language" element={
+        <>
+          <Header />
+          <Experience />
+          <Skills />
+          <Projects />
+          <Contact />
+        </>
+      } />
+    </Routes>
   )
 }
--- src/helpers/useDataJSON.js
+++ src/helpers/useDataJSON.js
@@ -1,9 +1,10 @@
 import data_es from '@/data/data_es.json';
+import data_en from '@/data/data_en.json';
 import { useParams } from 'react-router';
 
 export const useDataJson = () =>{
     const { language } = useParams();
     if(language == "es") return data_es
-    // if(language == "en") return data_es
+    if(language == "en") return data_en
     return data_es
 }
